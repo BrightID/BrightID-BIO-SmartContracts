08850c29895275827d35264f4b22f4527edc6343
Siftal <m.khanmohamadzadeh@gmail.com>
2019-05-28 14:14:23 -0400

    remove TokenURI

--- contracts/NonFungibleBIO.sol
+++ contracts/NonFungibleBIO.sol
@@ -16,34 +16,6 @@ contract NonFungibleBIO is ERC721Full, Ownable {
     }
 
     /**
-     * @dev Convert timestamp to string.
-     * @param timestamp The timestamp.
-     */
-    function timestamp2string(uint256 timestamp)
-        internal
-        pure
-        returns (string memory timestampString)
-    {
-        uint256 tmpTimestamp = timestamp; // To avoid security/no-assign-params error.
-        if (tmpTimestamp == 0) {
-            return "0";
-        }
-        uint256 i = tmpTimestamp;
-        uint256 len;
-        while (i != 0) {
-            len++;
-            i /= 10;
-        }
-        bytes memory bytesString = new bytes(len);
-        uint256 k = len - 1;
-        while (tmpTimestamp != 0) {
-            bytesString[k--] = byte(uint8(48 + tmpTimestamp % 10));
-            tmpTimestamp /= 10;
-        }
-        return string(bytesString);
-    }
-
-    /**
      * @dev Mint non fungible BIO token.
      * @param userAddress The BrightID user address.
      */
@@ -55,7 +27,6 @@ contract NonFungibleBIO is ERC721Full, Ownable {
         uint256 timestamp = now;
         string memory timestampString = timestamp2string(timestamp);
         super._mint(userAddress, tokenId);
-        super._setTokenURI(tokenId, timestampString);
         ++tokenId;
         return true;
     }
