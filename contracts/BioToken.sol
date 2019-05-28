52b4108e8d06e33d016455f1eb80674ec343cc51
Siftal <m.khanmohamadzadeh@gmail.com>
2019-05-28 15:22:16 -0400

    change ownership

--- contracts/BioToken.sol
+++ contracts/BioToken.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.5.0;
 
 import "./openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
-import "./openzeppelin/contracts/ownership/Ownable.sol";
 
 
 /**
  * @title BIO token contract.
   * @dev ERC20 token contract.
  */
-contract BioToken is ERC20Mintable, Ownable {
+contract BioToken is ERC20Mintable {
     string public constant name = "BrightIdOffering";
     string public constant symbol = "BIO";
     uint32 public constant decimals = 18;
