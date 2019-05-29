1cd7e8a6173b3481d1a9c59adeb0c1b0c3ba0ee2
Siftal <m.khanmohamadzadeh@gmail.com>
2019-05-29 03:08:04 -0400

    add title

--- contracts/NonFungibleBIO.sol
+++ contracts/NonFungibleBIO.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.5.0;
 import "./openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import "./openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 
+
+/**
+ * @title Non Fungible BIO token contract.
+  * @dev ERC721 token contract.
+ */
 contract NonFungibleBIO is ERC721Full, ERC721Mintable {
     string public constant NAME = "NonFungibleBIO";
     string public constant SYMBOL = "NFBIO";
